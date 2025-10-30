ef7e3b08440a01ceeaddc067ecf9cf080d525e83
Nanda S C <nandachowgle@gmail.com>
2025-10-30 15:30:38 +0530

    Update data_cleaning.py

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -14,16 +14,13 @@ def clean_data(data):
     """Clean the dataset"""
     print("\n🧹 Cleaning Data...")
 
-    # Drop duplicates
     data.drop_duplicates(inplace=True)
 
-    # Drop rows missing important info
     data.dropna(subset=['director', 'cast', 'country'], inplace=True)
 
     # Convert date_added to datetime
     data['date_added'] = pd.to_datetime(data['date_added'], errors='coerce')
 
-    # Fill missing values for other categorical columns
     data['rating'].fillna('Unknown', inplace=True)
     data['duration'].fillna('Unknown', inplace=True)
     data['listed_in'].fillna('Unknown', inplace=True)
